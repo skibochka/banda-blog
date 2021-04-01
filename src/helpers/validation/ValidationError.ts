3edb098ae5e548f5a226fd724bbc12aec621204b
skibochka <kiblyki@gmail.com>
2021-04-01 11:22:17 +0300

    refactor(validatormiddleware.ts): changed throwing validation error

--- src/helpers/validation/ValidationError.ts
+++
@@ -1,7 +0,0 @@
-export class ValidationError extends Error {
-  constructor(message) {
-    super();
-    this.message = message;
-    this.name = 'E_MISSING_OR_INVALID_PARAMS';
-  }
-}
--- src/middlewares/validatorMiddleware.ts
+++ src/middlewares/validatorMiddleware.ts
@@ -1,13 +1,14 @@
 import * as express from 'express';
 import * as Joi from 'joi';
-import { ValidationError } from '../helpers/validation/ValidationError';
+import { BadRequest } from 'http-errors';
 
 export function validatorMiddleware(validationSchema: any): express.RequestHandler {
   return (req, res, next) => {
     const bodyValidate = Joi.object(validationSchema).validate(req.body);
     const queryValidate = Joi.object(validationSchema).validate(req.query);
+
     if (bodyValidate.error && queryValidate.error) {
-      throw new ValidationError(bodyValidate.error.details);
+      throw new BadRequest('Validation Error!');
     }
 
     return next();
