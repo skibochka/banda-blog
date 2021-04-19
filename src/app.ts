5d8b08863d723f2da7a005c1f4fa0f052276bcbf
skibochka <kiblyki@gmail.com>
2021-04-19 15:22:56 +0300

    refactor: removed test route

--- src/app.ts
+++ src/app.ts
@@ -20,11 +20,6 @@ export const appPromise = (async (): Promise<express.Application> => {
   }));
   app.use(morgan('combined'));
 
-  // app.get('/test', (req, res, next) => {
-  //   return res.status(200).json({
-  //     msg: 'Test completed',
-  //   });
-  // });
   app.use('/auth', authRouter);
   app.use('/post', postRouter);
 
