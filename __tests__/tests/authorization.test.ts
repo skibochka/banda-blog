6cc221847b2e29ed5136321a716ab8de89cd3e3b
skibochka <kiblyki@gmail.com>
2021-04-26 12:28:06 +0300

    test: action test

--- __tests__/tests/authorization.test.ts
+++ __tests__/tests/authorization.test.ts
@@ -1,11 +1,16 @@
 import supertestInstance from '../../src/helpers/tests/supertestInstance';
+import supertest from 'supertest';
 
+let agent: supertest.SuperTest<supertest.Test>;
 let access: string;
 let refresh: string;
 
 describe('Authorization tests', () => {
-  test('Sign-up test', async (done) => {
-    const agent = await supertestInstance();
+  beforeAll(async () => {
+    agent = await supertestInstance();
+  });
+
+  test('Sign-up test', (done) => {
     agent
       .post('/auth/sign-up')
       .send({
@@ -20,8 +25,7 @@ describe('Authorization tests', () => {
       });
   });
 
-  test('Sign-in test', async (done) => {
-    const agent = await supertestInstance();
+  test('Sign-in test', (done) => {
     agent
       .post('/auth/sign-in')
       .send({
@@ -39,8 +43,7 @@ describe('Authorization tests', () => {
       });
   });
 
-  test('Sign-out test', async (done) => {
-    const agent = await supertestInstance();
+  test('Sign-out test', (done) => {
     agent
       .post('/auth/sign-out')
       .set('Authorization', `Bearer ${access}`)
