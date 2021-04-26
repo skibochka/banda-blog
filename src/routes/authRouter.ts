eda4c011eab59c82d139e89ec129a040019dbf43
skibochka <kiblyki@gmail.com>
2021-04-26 15:18:20 +0300

    test: test

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import eah from 'express-async-handler';
 import { authValidation } from '../validation/authSchemas';
-// import { authMiddleware } from '../middlewares/authMiddleware';
+import { authMiddleware } from '../middlewares/authMiddleware';
 import { signUp, signIn, signOut } from '../controllers/authController';
 import { validatorMiddleware } from '../middlewares/validatorMiddleware';
 
@@ -12,7 +12,7 @@ authRouter.post('/sign-up', validatorMiddleware(authValidation.checkUser), eah(s
 
 authRouter.post('/sign-in', validatorMiddleware(authValidation.checkUser), eah(signIn));
 
-// authRouter.use(eah(authMiddleware));
+authRouter.use(eah(authMiddleware));
 
 authRouter.post('/sign-out', validatorMiddleware(authValidation.logoutUser), eah(signOut));
 
